c5748ac8acf0bf76e26364e641bbbcc5b96cbb71
Facundo Cabrera <cabrerafacundo@gmail.com>
2013-08-11 21:04:11 -0300

    Integrated grunt release (Issue #20)

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(grunt) {
+  // grunt.initConfig({
+  //   release: {
+  //     options: {
+  //       npmtag: true,
+  //     }
+  //   }
+  // });
+
+  grunt.loadNpmTasks('grunt-release');
+};
